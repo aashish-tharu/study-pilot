0650cdecb60136c95f0149d710fa8bb5bd0ccf17
Aashish Tharu <aashishtharugh@gmail.com>
2026-07-29 20:58:12 +0530

    custom api response and error handling

---
+++ backend/src/utils/APIResponse.js
@@ -0,0 +1,7 @@
+export class APIResponse {
+    constructor(statusCode, data, message="Success") {
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
